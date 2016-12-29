6afc3075319fdae99021247b3e0ff879f092d900
ArmanceB <armance.bourquin@orange.fr>
2016-12-29 12:47:34 +0100

    Update fft.py

--- fft.py
+++ fft.py
@@ -1,22 +1,19 @@
-i=complex(0,1)
-import numpy as np
-
+from cmath import exp
+from cmath import pi
 
 def fft_recursive(A):
     n=int(len(A))
     if n==1:
         return(A)
-    wn=np.exp(2*i*np.pi/n)
+    wn=exp(2*pi*1j/n)
     w=1
-    A0=np.array([A[2*k] for k in range(0,int(n/2))])
-    A1=np.array([A[2*k+1] for k in range(0,int(n/2))])
+    A0=[A[2*k] for k in range(0,int(n/2))]
+    A1=[A[2*k+1] for k in range(0,int(n/2))]
     Y0=fft_recursive(A0)
     Y1=fft_recursive(A1)
-    Y=np.zeros(n)
-    for k in range(int(n/2)):
+    Y=[0 for i in range(n)]
+    for k in range(0,int(n/2)):
         Y[k]=Y0[k]+w*Y1[k]
-        Y[k+n/2]=Y0[k]-w*Y1[k]
+        Y[k+int(n/2)]=Y0[k]-w*Y1[k]
         w=w*wn
     return(Y)
-        
-        
